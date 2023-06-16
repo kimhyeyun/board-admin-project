225be5f00d6e760033ab20c957be128eb52e7271
Kimhyeyun <cqqwer@gmail.com>
2023-06-16 18:53:17 +0900

    [ADD] 스프링 시큐리티 인증 데이터 작성
    `Principal`, 카카오 인증 api 의 응답, 그리고 테스트 작성

---
+++ src/main/java/dev/be/boardadminproject/dto/security/BoardAdminPrincipal.java
@@ -0,0 +1,114 @@
+package dev.be.boardadminproject.dto.security;
+
+
+import dev.be.boardadminproject.domain.constant.RoleType;
+import dev.be.boardadminproject.dto.MemberDto;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public record BoardAdminPrincipal(
+        String username,
+        String password,
+        Collection<? extends GrantedAuthority> authorities,
+        String email,
+        String nickname,
+        String memo,
+        Map<String, Object> oAuth2Attributes
+) implements UserDetails, OAuth2User {
+
+    public static BoardAdminPrincipal of(String username, String password, Set<RoleType> roleTypes, String email, String nickname, String memo) {
+        return BoardAdminPrincipal.of(username, password, roleTypes, email, nickname, memo, Map.of());
+    }
+
+    public static BoardAdminPrincipal of(String username, String password, Set<RoleType> roleTypes, String email, String nickname, String memo, Map<String, Object> oAuth2Attributes) {
+        return new BoardAdminPrincipal(
+                username,
+                password,
+                roleTypes.stream()
+                        .map(RoleType::getRoleName)
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toUnmodifiableSet()),
+                email,
+                nickname,
+                memo,
+                oAuth2Attributes
+        );
+    }
+
+    public static BoardAdminPrincipal from(MemberDto dto) {
+        return BoardAdminPrincipal.of(
+                dto.getMemberId(),
+                dto.getPassword(),
+                dto.getRoleTypes(),
+                dto.getEmail(),
+                dto.getNickname(),
+                dto.getMemo()
+        );
+    }
+
+    public MemberDto toDto() {
+        return MemberDto.builder()
+                .memberId(username)
+                .password(password)
+                .roleTypes(authorities.stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .map(RoleType::valueOf)
+                        .collect(Collectors.toUnmodifiableSet()))
+                .email(email)
+                .nickname(nickname)
+                .memo(memo)
+                .build();
+    }
+
+    @Override
+    public String getName() {
+        return username;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return oAuth2Attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
