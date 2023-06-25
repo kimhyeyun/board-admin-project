6d9becc9405b44796b019f6718a759877cd150dd
Kimhyeyun <cqqwer@gmail.com>
2023-06-25 15:57:59 +0900

    [ADD] 어드민 회원 API의 응답 객체 설계

--- src/main/java/dev/be/boardadminproject/dto/AdminAccountDto.java
+++ src/main/java/dev/be/boardadminproject/dto/AdminAccountDto.java
@@ -4,46 +4,79 @@ import dev.be.boardadminproject.domain.AdminAccount;
 import dev.be.boardadminproject.domain.constant.RoleType;
 import lombok.Builder;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-@Builder
-public record AdminAccountDto(
-        String memberId,
-        String password,
-        Set<RoleType> roleTypes,
-        String email,
-        String nickname,
-        String memo,
-        LocalDateTime createdAt,
-        String createdBy,
-        LocalDateTime modifiedAt,
-        String modifiedBy
-) {
+public class AdminAccountDto implements Serializable {
 
-    public static AdminAccountDto from(AdminAccount entity) {
-        return AdminAccountDto.builder()
-                .memberId(entity.getMemberId())
-                .password(entity.getPassword())
-                .roleTypes(entity.getRoleTypes())
-                .email(entity.getEmail())
-                .nickname(entity.getNickname())
-                .memo(entity.getMemo())
-                .createdAt(entity.getCreatedAt())
-                .createdBy(entity.getCreatedBy())
-                .modifiedAt(entity.getModifiedAt())
-                .modifiedBy(entity.getModifiedBy())
-                .build();
+    @Builder
+    public record Dto(
+            String memberId,
+            String password,
+            Set<RoleType> roleTypes,
+            String email,
+            String nickname,
+            String memo,
+            LocalDateTime createdAt,
+            String createdBy,
+            LocalDateTime modifiedAt,
+            String modifiedBy
+    ) {
+
+        public static AdminAccountDto.Dto from(AdminAccount entity) {
+            return AdminAccountDto.Dto.builder()
+                    .memberId(entity.getMemberId())
+                    .password(entity.getPassword())
+                    .roleTypes(entity.getRoleTypes())
+                    .email(entity.getEmail())
+                    .nickname(entity.getNickname())
+                    .memo(entity.getMemo())
+                    .createdAt(entity.getCreatedAt())
+                    .createdBy(entity.getCreatedBy())
+                    .modifiedAt(entity.getModifiedAt())
+                    .modifiedBy(entity.getModifiedBy())
+                    .build();
+        }
+
+        public AdminAccount toEntity() {
+            return AdminAccount.builder()
+                    .memberId(memberId)
+                    .password(password)
+                    .roleTypes(roleTypes)
+                    .email(email)
+                    .nickname(nickname)
+                    .memo(memo)
+                    .build();
+        }
     }
 
-    public AdminAccount toEntity() {
-        return AdminAccount.builder()
-                .memberId(memberId)
-                .password(password)
-                .roleTypes(roleTypes)
-                .email(email)
-                .nickname(nickname)
-                .memo(memo)
-                .build();
+    @Builder
+    public record Response(
+            String memberId,
+            String roleTypes,
+            String email,
+            String nickname,
+            String memo,
+            LocalDateTime createdAt,
+            String createdBy
+    ) {
+
+        public static Response from(Dto dto) {
+            return Response.builder()
+                    .memberId(dto.memberId)
+                    .roleTypes(dto.roleTypes.stream()
+                            .map(RoleType::getDescription)
+                            .collect(Collectors.joining(", ")))
+                    .email(dto.email)
+                    .nickname(dto.nickname)
+                    .memo(dto.memo)
+                    .createdAt(dto.createdAt)
+                    .createdBy(dto.createdBy)
+                    .build();
+        }
+        
     }
+
 }
